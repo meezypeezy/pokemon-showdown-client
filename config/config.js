0bb909ea11d9730acbc4ca89b5b50a7979e9f20a
meezypeezy <72401075+meezypeezy@users.noreply.github.com>
2022-01-29 17:17:48 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -19,7 +19,7 @@ Config.whitelist = [
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
 	id: 'dragonheaven',
-	host: 'https://smash-roulette.herokuapp.com',
+	host: 'smash-roulette.herokuapp.com',
 	port: 443,
 	httpport: 8000,
 	altport: 80,
