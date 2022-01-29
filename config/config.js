e4b38611556edaf789c0c50862dc503cdb967912
meezypeezy <72401075+meezypeezy@users.noreply.github.com>
2022-01-29 16:36:38 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -19,7 +19,7 @@ Config.whitelist = [
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
 	id: 'dragonheaven',
-	host: 'localhost:8000',
+	host: 'https://github.com/meezypeezy/DH',
 	port: 443,
 	httpport: 8000,
 	altport: 80,
